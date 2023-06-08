7b3c942ddfa1c42359b0a3da0fc913e0e9d3bd9e
Maxime Talon <maximetalon@hotmail.com>
2023-06-08 07:53:56 -0400

    Register SSubTmr6.dll

--- PortLibrary/register_ocx.bat
+++ PortLibrary/register_ocx.bat
@@ -1,4 +1,5 @@
 regsvr32.exe %~dp0vbalExpBar6.ocx
 regsvr32.exe %~dp0cPopMenu6.ocx
 regsvr32.exe %~dp0timermm.dll
+regsvr32.exe %~dp0SSubTmr6.dll
 @pause
--- PortLibrary/unregister_ocx.bat
+++ PortLibrary/unregister_ocx.bat
@@ -1,4 +1,5 @@
 regsvr32.exe /U %~dp0vbalExpBar6.ocx
 regsvr32.exe /U %~dp0cPopMenu6.ocx
 regsvr32.exe /U %~dp0timermm.dll
+regsvr32.exe /U %~dp0SSubTmr6.dll
 @pause
